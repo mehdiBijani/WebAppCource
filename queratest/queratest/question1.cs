8b0cc947efdfedc36178d41fb1572fffb0402a93
Mehdi <m.bijani7@gmail.com>
2025-04-02 12:24:47 +0330

    quera question

---
+++ queratest/queratest/question1.cs
@@ -0,0 +1,26 @@
+﻿void PrintStars(string input)
+{
+    var splitedInput = input.ToCharArray();
+
+    var evenLines = string.Empty;
+    var oddLines = string.Empty;
+    foreach (var item in splitedInput)
+    {
+        if (item.Equals('0'))
+        {
+            evenLines += "***";
+            oddLines += "*.*";
+        }
+        else
+        {
+            evenLines += ".*.";
+            oddLines += ".*.";
+        }
+    }
+    
+    Console.WriteLine(evenLines + "\n" + oddLines + "\n" + evenLines);
+}
+
+PrintStars("0101");
+PrintStars("10011");
+PrintStars("0101");
