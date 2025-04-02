ed71b955258ac3ffd8448e778ec874234ab1f1a6
Mehdi <m.bijani7@gmail.com>
2025-04-02 12:42:31 +0330

    Start Project EfCore Sample

---
+++ EfCore/EfCore_Domain/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EfCore_Domain.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
